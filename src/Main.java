9fbc052f2e5ed6d443db4c92ceb051e02cf11062
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-09-27 13:03:49 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int inputAngka;
+        String inputCommand;
+
+        do{
+            System.out.print("Masukkan angka : ");
+            inputAngka = input.nextInt();
+            if(inputAngka % 2 == 0){
+                System.out.println(inputAngka + " adalah Genap");
+            }else{
+                System.out.println(inputAngka + " adalah Ganjil");
+            }
+
+            System.out.println("");
+            System.out.print("Lagi (Y / N)? : ");
+            inputCommand = input.next();
+        }while (
+                inputCommand.equalsIgnoreCase("y")
+        );
+
+
+    }
+}

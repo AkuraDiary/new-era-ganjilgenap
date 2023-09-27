cc1a9be0a74e6106adda45c8c33d115cbeeb8fab
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-09-27 14:02:50 +0700

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,7 @@ public class Main {
                 System.out.println(inputAngka + " adalah Ganjil");
             }
 
-            System.out.println("");
+            System.out.println();
             System.out.print("Lagi (Y / N)? : ");
             inputCommand = input.next();
         }while (

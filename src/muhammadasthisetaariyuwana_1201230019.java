9b5285ad7b44a484738727944624fe368af7a915
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-09-28 09:14:26 +0700

    add screenshot

--- src/muhammadasthisetaariyuwana_1201230019.java
+++ src/muhammadasthisetaariyuwana_1201230019.java
@@ -23,5 +23,22 @@ public class muhammadasthisetaariyuwana_1201230019 {
         );
 
 
+//        while(true){
+//
+//            try{
+//                System.out.print("Masukkan angka : ");
+//                inputAngka = input.nextInt();
+//
+//                if(inputAngka % 2 == 0){
+//                    System.out.println(inputAngka + " adalah Genap");
+//                }else{
+//                    System.out.println(inputAngka + " adalah Ganjil");
+//                }
+//            }catch ( Exception e){
+//                break;
+//
+//            }
+//
+//        }
     }
 }

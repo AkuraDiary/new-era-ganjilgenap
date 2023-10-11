32202efa98d1376a76e8513a7a6ff93b6e6d516d
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-11 13:24:20 +0700

    upload

--- src/muhammadasthisetaariyuwana_1201230019.java
+++ src/muhammadasthisetaariyuwana_1201230019.java
@@ -5,15 +5,13 @@ public class muhammadasthisetaariyuwana_1201230019 {
         Scanner input = new Scanner(System.in);
         int inputAngka;
         String inputCommand;
+        muhammadasthisetaariyuwana_1201230019 myClass = new muhammadasthisetaariyuwana_1201230019();
 
         do{
             System.out.print("Masukkan angka : ");
             inputAngka = input.nextInt();
-            if(inputAngka % 2 == 0){
-                System.out.println(inputAngka + " adalah Genap");
-            }else{
-                System.out.println(inputAngka + " adalah Ganjil");
-            }
+
+            myClass.cekGanjilGenap(inputAngka);
 
             System.out.print("Lagi (Y / N)? : ");
             inputCommand = input.next();
@@ -40,5 +38,14 @@ public class muhammadasthisetaariyuwana_1201230019 {
 //            }
 //
 //        }
+    }
+
+    void cekGanjilGenap(int inputAngka){
+
+        if(inputAngka % 2 == 0){
+            System.out.println(inputAngka + " adalah Genap");
+        }else{
+            System.out.println(inputAngka + " adalah Ganjil");
+        }
     }
 }
